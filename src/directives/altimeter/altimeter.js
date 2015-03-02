87c748dbc61a9c45281d58cd4b8f961a21175a8e
indev <kristian@indev.se>
2015-03-02 21:34:40 +0100

    Implementation of altimeter

--- src/directives/altimeter/altimeter.js
+++ src/directives/altimeter/altimeter.js
@@ -1,16 +1,32 @@
 angular.module('hk-aerial-gui').directive('altimeter', function () {
   'use strict';
 
+  function getTotalAltitudeString(totalAltitude) {
+    var result = '';
+    var numChars = totalAltitude.toString().length;
+    var desiredChars = 6;
+    for ( var i = numChars; i < desiredChars; ++i )
+      result += '0';
+
+    return result + totalAltitude;
+  }
+
+  function getNeedleAngle(totalAltitude) {
+    var circleMod = 100;
+    return (totalAltitude / circleMod) * 360;
+  }
+
   return {
     restrict: 'E',
     replace: true,
     scope: {
-
+      altitude: '='
     },
     templateUrl: 'directives/altimeter/altimeter.html',
     link: function (scope, element, attrs, fn) {
 
-
+      scope.totalAltitude = getTotalAltitudeString(scope.altitude);
+      scope.needleRotation = getNeedleAngle(scope.altitude);
     }
   };
 });
